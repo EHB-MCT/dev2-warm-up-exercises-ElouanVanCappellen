0f65e7b6f2be1cc95af21b71c67c00dd1e9f3a89
Elouan Van Cappellen <elouan.van.cappellen@student.ehb.be>
2025-02-13 11:23:03 +0100

    klaar met warmup 1

--- main.js
+++ main.js
@@ -5,9 +5,34 @@ import {
 } from './lib.js';
 
 runTitle();
+warmup1();
 
 function warmup1() {
     console.log("Exercise week 1");
+    // addEventListener('click',changeText);
+    document.getElementById("button-1a").onclick = changeText;
+    document.getElementById("button-1b").onclick = changeColor;
+    document.getElementById("button-1c").onclick = newBoldText;
+}
+
+function changeText() {
+
+    document.getElementById("content-1").innerHTML = "Hallo, dit is de oefening van Elouan Van Cappellen";
+}
+
+function changeColor() {
+
+    const container1 = document.querySelector("#section-1");
+
+    container1.style.background = "lightblue";
+}
+
+function newBoldText() {
+
+    const newElement = document.createElement("h1");
+    newElement.innerHTML = "Ik ben er klaar voor!";
+
+    document.getElementById("content-1").append(newElement);
 }
 
 function warmup2() {
